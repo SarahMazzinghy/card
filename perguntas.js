dbc3b12cf1725a72a904a463274a602035cc01c7
SarahMazzinghy <00001101851892sp@al.educacao.sp.gov.br>
2024-11-25 11:57:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Historia',
+    'O que é o tratado de tordesilhas?',
+    'foi um acordo assinado em 1494 entre os reinos de Portugal e Castela, que estabeleceu a divisão do mundo extra-europeu entre as duas nações'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'A capital da Alemanha é Berlim'
+)
+
+criaCartao(
+    'Pop',
+    'Quais são as 3 reliquias da Morte em Harry Potter?',
+    'A Varinha das Varinhas, A Pedra da Ressurreição, A Capa da Invisibilidade'
+)
+
+criaCartao(
+    'Ingês',
+    'Como se diz agua em Inglês?',
+    'Water'
+)
